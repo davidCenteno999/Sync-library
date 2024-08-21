c4181a7ecc5cf9c7771bcdfcc8f4eddd514d3e3f
David Centeno Araya <100227276+davidCenteno999@users.noreply.github.com>
2024-08-20 21:10:21 -0600

    example changes

--- example_semaphore.c
+++ example_semaphore.c
@@ -4,29 +4,54 @@
 #include "semaphore.h"
 
 semaphore sem;  // Cambiado de puntero a instancia
+int num = 0;
+
+typedef struct {
+  int thread_id;
+  int op1;
+  int op2;
+  char operation; 
+
+} args_f1;
+
 
 void *f1(void* args) {
+
     wait(&sem);
+     args_f1 *t_args = (args_f1*)args;
+    printf("Hilo %d\n", t_args->thread_id);
+    printf("Operacion: %c || Operando 1: %d || Operando 2: %d \n",
+    t_args->operation,t_args->op1,t_args->op2);
+    int result;
+    if(t_args->operation == '+'){
+        result = t_args->op1 + t_args->op2;
+    }else{
+        result = t_args->op1 - t_args->op2;
+    }
+    printf("Resultado: %d\n", result);
     sleep(1);
-    printf("Hilo %d\n", *(int*)args);
     signal(&sem);
     free(args);  // Liberar la memoria asignada para el argumento
     return NULL;
 }
 
 int main(void) {
-    //printf("Hello World\n");
+    srand(time(NULL));
 
     // Inicializar el semáforo
     semaphore_init(&sem, 1);
 
     pthread_t th[3];
-
+    
     // Crear 3 hilos
     for (int i = 0; i < 3; i++) {
-        int *a = malloc(sizeof(int)); 
-        *a = i;
-        if (pthread_create(&th[i], NULL, f1, a) != 0) {
+        
+        args_f1 *args = malloc(sizeof(args_f1)); 
+        args->thread_id = i;
+        args->op1 = random() % 100;
+        args->op2 = random() % 100;
+        args->operation = (rand() % 2 == 0) ? '+' : '-';
+        if (pthread_create(&th[i], NULL, f1, args) != 0) {
             printf("Error al crear el hilo\n");
         }
     }
@@ -41,5 +66,3 @@ int main(void) {
 
     return 0;
 }
-
-
